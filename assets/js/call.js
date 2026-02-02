af2eb2a01f52696b115f54a8224e7e4b6a0bdaf0
jbarnett <jbarnett@10.0.2.3>
2026-02-02 10:23:17 -0700

    migrated to external JS

---
+++ assets/js/call.js
@@ -0,0 +1,57 @@
+// import { ElevenLabsCall} from './conversation.js';
+
+let activeCall = null;
+let pc;
+let audioEl = document.createElement("audio");
+audioEl.autoplay = true;
+
+function startCall(agentId, beginConvoLabel, endConvoLabel) {
+
+    if (activeCall === null) {
+        console.log("NO active call, gonna start it");
+        const button = document.getElementById("playVideoLink");
+        button.textContent = endConvoLabel;
+        activeCall = new window.ElevenLabsCall(agentId, beginConvoLabel);
+
+        // activeCall.on("disconnected", () => {
+        //   console.log("Call disconnected (silence or network issue)");
+        //   conversationEnded(beginConvoLabel);
+        // });
+        //
+        // activeCall.on("ended", () => {
+        //   console.log("Call ended");
+        //   conversationEnded(beginConvoLabel);
+        // });
+
+        activeCall.start();
+    } else {
+        conversationEnded(beginConvoLabel);
+        // console.log("there is an active call, gonna stop it");
+        // activeCall.stop();
+        // // encCall();
+        // activeCall = null;
+        // const button = document.getElementById("playVideoLink");
+        // button.textContent = beginConvoLabel;
+    }
+}
+
+function conversationEnded(beginConvoLabel) {
+    if (activeCall != null) {
+        console.log("there is an active call, gonna stop it");
+        activeCall.stop();
+        // encCall();
+        activeCall = null;
+        const button = document.getElementById("playVideoLink");
+        button.textContent = beginConvoLabel;
+    }
+}
+
+function endCall() {
+    // document.getElementById("playVideoLink").disabled = false;
+    // document.getElementById("endCall").disabled = true;
+
+    if (pc) {
+        pc.close();
+        pc = null;
+    }
+}
